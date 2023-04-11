660ed4d76be3169c1f60ced3d3bf8749a68110e8
dariuszKlimko <dariuszklimkoportfolio@gmail.com>
2023-04-11 16:55:19 +0200

    chore: tests for profiles controller added

--- app/test/controllers/profiles.e2e-spec.ts
+++ app/test/controllers/profiles.e2e-spec.ts
@@ -1,81 +1,104 @@
-// import { Test, TestingModule } from "@nestjs/testing";
-// import { INestApplication, HttpStatus, ValidationPipe } from "@nestjs/common";
-// import * as request from "supertest";
-// import { AppModule } from "@app/app.module";
-// import loadFixtures, { FixtureFactory } from "@test/helpers/loadFixtures";
-// import { Repository } from "typeorm";
-// import { User } from "@app/entities/user.entity";
-// import { CreateUserDto } from "@app/dtos/user/create-user.dto";
-// import { Measurement } from "@app/entities/measurement.entity";
-// import { userRegister } from "@test/helpers/userRegister";
-// import { userLogin } from "@test/helpers/userLogin";
-// import { Profile } from "@app/entities/profile.entity";
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication, HttpStatus, ValidationPipe } from "@nestjs/common";
+import * as request from "supertest";
+import { AppModule } from "@app/app.module";
+import loadFixtures, { FixtureFactory } from "@test/helpers/loadFixtures";
+import { Repository } from "typeorm";
+import { userLogin } from "@test/helpers/userLogin";
+import { Profile } from "@app/entities/profile.entity";
 
-// describe("Profiles (e2e)", () => {
-//   let app: INestApplication;
-//   let fixtures: FixtureFactory;
-//   let userRepository: Repository<User>;
-//   let measurementRepository: Repository<Measurement>;
-//   let profileRepository: Repository<Profile>;
-//   let user2accessToken: string;
-//   let user3accessToken: string;
-//   let user4accessToken: string;
+describe("Profiles (e2e)", () => {
+  let app: INestApplication;
+  let fixtures: FixtureFactory;
+  let profileRepository: Repository<Profile>;
+  let profile1accessToken: string;
+  let profile2accessToken: string;
+  let profile3accessToken: string;
 
-//   beforeAll(async () => {
-//     fixtures = await loadFixtures();
-//     const moduleFixture: TestingModule = await Test.createTestingModule({
-//       imports: [AppModule],
-//     }).compile();
+  beforeAll(async () => {
+    fixtures = await loadFixtures();
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
 
-//     userRepository = moduleFixture.get("UserRepository");
-//     measurementRepository = moduleFixture.get("MeasurementRepository");
-//     profileRepository = moduleFixture.get("ProfileRepository");
+    profileRepository = moduleFixture.get("ProfileRepository");
 
-//     app = moduleFixture.createNestApplication();
-//     app.useGlobalPipes(new ValidationPipe());
-//     await app.init();
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init();
 
-//     user2accessToken = await userLogin("user2@email.com", "Qwert12345!", app).then((res) => res.body.accessToken);
-//     user3accessToken = await userLogin("user3@email.com", "Qwert12345!", app).then((res) => res.body.accessToken);
-//     user4accessToken = await userLogin("user4@email.com", "Qwert12345!", app).then((res) => res.body.accessToken);
-//   });
+    profile1accessToken = await userLogin("profile1@email.com", "Qwert12345!", app).then(
+      (res) => res.body.accessToken
+    );
+    profile2accessToken = await userLogin("profile2@email.com", "Qwert12345!", app).then(
+      (res) => res.body.accessToken
+    );
+    profile3accessToken = await userLogin("profile3@email.com", "Qwert12345!", app).then(
+      (res) => res.body.accessToken
+    );
+  });
 
-//   describe("/users (PATCH) - update user's data", () => {
-//     it("should update user height in database for given accessToken if height is number", async () => {
-//       await request
-//         .default(app.getHttpServer())
-//         .patch("/users")
-//         .set("Authorization", `Bearer ${user4accessToken}`)
-//         .send({ height: 180 })
-//         .then((res) => {
-//           expect(res.status).toEqual(HttpStatus.OK);
-//           expect(res.body.height).toEqual(180);
-//         });
+  describe("/profiles (PATCH) - update profile's data", () => {
+    it("should update profile height in database for given accessToken if height is number", async () => {
+      let userId: string;
+      await request
+        .default(app.getHttpServer())
+        .patch("/profiles")
+        .set("Authorization", `Bearer ${profile1accessToken}`)
+        .send({ height: 180 })
+        .then((res) => {
+          expect(res.status).toEqual(HttpStatus.OK);
+          expect(res.body.height).toEqual(180);
+          userId = res.body.userId;
+        });
 
-//       return userRepository.findOneBy({ email: "user4@email.com" }).then((user) => {
-//         expect(user.height).toEqual(180);
-//       });
-//     });
+      return profileRepository.findOneBy({ userId }).then((profile) => {
+        expect(profile.height).toEqual(180);
+      });
+    });
 
-//     it("should not update user height in database for given accessToken if height is number", () => {
-//       return request
-//         .default(app.getHttpServer())
-//         .patch("/users")
-//         .set("Authorization", "Bearer someToken")
-//         .then((res) => {
-//           expect(res.status).toEqual(HttpStatus.UNAUTHORIZED);
-//         });
-//     });
+    it("should not update profile height in database for given accessToken if height is number", () => {
+      return request
+        .default(app.getHttpServer())
+        .patch("/profiles")
+        .set("Authorization", "Bearer someToken")
+        .then((res) => {
+          expect(res.status).toEqual(HttpStatus.UNAUTHORIZED);
+        });
+    });
 
-//     it("should not update user height in database for given accessToken if height is not number", () => {
-//       return request
-//         .default(app.getHttpServer())
-//         .patch("/users")
-//         .set("Authorization", `Bearer ${user4accessToken}`)
-//         .send({ height: "180" })
-//         .then((res) => {
-//           expect(res.status).toEqual(HttpStatus.BAD_REQUEST);
-//         });
-//     });
-//   });
-// });
+    it("should not update profile height in database for given accessToken if height is not number", () => {
+      return request
+        .default(app.getHttpServer())
+        .patch("/profiles")
+        .set("Authorization", `Bearer ${profile2accessToken}`)
+        .send({ height: "180" })
+        .then((res) => {
+          expect(res.status).toEqual(HttpStatus.BAD_REQUEST);
+        });
+    });
+  });
+
+  describe("/profiles (GET) - get profile's data", () => {
+    it("should get profile height for given accessToken", () => {
+      return request
+        .default(app.getHttpServer())
+        .get("/profiles")
+        .set("Authorization", `Bearer ${profile3accessToken}`)
+        .then((res) => {
+          expect(res.status).toEqual(HttpStatus.OK);
+          expect(res.body.height).toEqual(fixtures.get("profile3").height);
+        });
+    });
+
+    it("should not get profile height for invalid accessToken", () => {
+      return request
+        .default(app.getHttpServer())
+        .get("/profiles")
+        .set("Authorization", "Bearer someToken")
+        .then((res) => {
+          expect(res.status).toEqual(HttpStatus.UNAUTHORIZED);
+        });
+    });
+  });
+});
