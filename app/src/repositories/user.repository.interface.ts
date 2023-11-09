0528cde82d2397d1c61ea91006e37beb2b2f4229
dariuszKlimko <dariuszklimkoportfolio@gmail.com>
2023-11-09 23:54:17 +0100

    new structure

--- app/src/repositories/user.repository.interface.ts
+++
@@ -1,7 +0,0 @@
-import { BaseInterfaceRepository } from "@app/common/repository/base.interface.repository";
-import { CreateUserDto } from "@app/dtos/user/create-user.dto";
-import { User } from "@app/entities/user.entity";
-
-export interface UserRepositoryInterface extends BaseInterfaceRepository<User, CreateUserDto, null> {
-
-}
