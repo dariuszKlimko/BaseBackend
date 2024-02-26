8c0231b49ad2de96c3d3a21130a617dcee7f68b9
dariuszKlimko <dariuszklimkoportfolio@gmail.com>
2024-02-26 13:18:47 +0100

    format and lint

--- app/src/services/math.service.spec.ts
+++ app/src/services/math.service.spec.ts
@@ -6,8 +6,8 @@ describe("MathService", () => {
   let mathService: MathServiceIntrface;
 
   beforeAll(async () => {
-    const module: TestingModule = await Test.createTestingModule({ 
-        providers: [MathSevice],
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [MathSevice],
     }).compile();
 
     mathService = module.get<MathServiceIntrface>(MathSevice);
@@ -35,24 +35,23 @@ describe("MathService", () => {
 
   describe("powValue()", () => {
     it("should return value to the power of 1", async () => {
-      const result: number = mathService.powValue(3,1);
+      const result: number = mathService.powValue(3, 1);
       return expect(result).toEqual(3);
     });
 
     it("should return value to the power of 2", async () => {
-      const result: number = mathService.powValue(3,2);
+      const result: number = mathService.powValue(3, 2);
       return expect(result).toEqual(9);
     });
 
     it("should return value to the power of 3", async () => {
-      const result: number = mathService.powValue(3.14,3);
+      const result: number = mathService.powValue(3.14, 3);
       return expect(result).toEqual(30.959144000000002);
     });
 
     it("should return value to the power of 0.5", async () => {
-      const result: number = mathService.powValue(4,0.5);
+      const result: number = mathService.powValue(4, 0.5);
       return expect(result).toEqual(2);
     });
   });
-
 });
